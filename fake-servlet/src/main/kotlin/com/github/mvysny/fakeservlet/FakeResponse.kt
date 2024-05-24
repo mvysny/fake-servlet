27601cdb9e47306f808fc214b4b9bb7804410b83
Martin Vysny <mavi@vaadin.com>
2024-05-24 14:48:39 +0300

    minor

--- fake-servlet/src/main/kotlin/com/github/mvysny/fakeservlet/FakeResponse.kt
+++ fake-servlet/src/main/kotlin/com/github/mvysny/fakeservlet/FakeResponse.kt
@@ -2,7 +2,6 @@ package com.github.mvysny.fakeservlet
 
 import org.slf4j.LoggerFactory
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import java.io.OutputStreamWriter
 import java.io.PrintWriter
 import java.nio.charset.Charset
--- fake-servlet5/src/main/kotlin/com/github/mvysny/fakeservlet/FakeResponse.kt
+++ fake-servlet5/src/main/kotlin/com/github/mvysny/fakeservlet/FakeResponse.kt
@@ -1,6 +1,5 @@
 package com.github.mvysny.fakeservlet
 
-import java.io.IOException
 import java.io.PrintWriter
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
