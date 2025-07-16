0d595b8fb18a41ace4d1d9f8c9bfca4386281d00
Martin Vysny <mavi@vaadin.com>
2025-07-16 07:09:48 +0300

    minor

--- fake-servlet5/src/main/kotlin/com/github/mvysny/fakeservlet/ServletInputStreamImpl.kt
+++ fake-servlet5/src/main/kotlin/com/github/mvysny/fakeservlet/ServletInputStreamImpl.kt
@@ -15,25 +15,18 @@ public class ServletInputStreamImpl(public val delegate: ByteArrayInputStream) :
     }
     override fun read(): Int = delegate.read()
     override fun read(b: ByteArray): Int = delegate.read(b)
-    override fun read(
-        b: ByteArray,
-        off: Int,
-        len: Int
-    ): Int = delegate.read(b, off, len)
+    override fun read(b: ByteArray, off: Int, len: Int): Int = delegate.read(b, off, len)
 
     override fun skip(n: Long): Long = delegate.skip(n)
     override fun available(): Int = delegate.available()
     override fun close() {
         delegate.close()
     }
-
     override fun mark(readlimit: Int) {
         delegate.mark(readlimit)
     }
-
     override fun reset() {
         delegate.reset()
     }
-
     override fun markSupported(): Boolean = delegate.markSupported()
 }
