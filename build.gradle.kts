36b6eee92735ff68439863f4a78836b1411a9fd6
Martin Vysny <mavi@vaadin.com>
2025-07-16 07:39:23 +0300

    1.4-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.fake-servlet"
-    version = "1.3"
+    version = "1.4-SNAPSHOT"
 
     repositories {
         mavenCentral()
